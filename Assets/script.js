a4ee0c84514267a5dd44e4d6e4fcb2cf386dd79e
Nicholas Herold <nherold86@gmail.com>
2021-05-18 21:44:34 -0400

    first commit for project started html css and js

---
+++ Assets/script.js
@@ -0,0 +1,46 @@
+var button1 = document.querySelector("#button1");
+console.log(button1);
+var questionString = document.querySelector("#buttons")
+
+var question1 = {
+    question: 'How many fruits?',
+    answer1: 2,
+    answer2: 3,
+    answer3: 4,
+    answer4: 1
+}
+
+var question2 = {
+    question: 'How many cars?',
+    answer1: 2,
+    answer2: 3,
+    answer3: 15,
+    answer4: 1
+}
+
+var question3 = {
+    question: 'How many cans?',
+    answer1: 2,
+    answer2: 20,
+    answer3: 4,
+    answer4: 1
+}
+var question4 = {
+    question: 'How many people?',
+    answer1: 50,
+    answer2: 3,
+    answer3: 4,
+    answer4: 1
+}
+
+var questionsAnswers = [
+    question1,question2,question3,question4
+]
+
+console.log(questionsAnswers[0]);
+
+function cycleQuestions() {
+for (let i = 0; i < questionsAnswers.length; i++) {
+  
+}
+}
